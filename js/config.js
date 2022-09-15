d91857ce39ba0d69c02994a0c4ee652311f92b69
Rafael Fiuza <61255847+V1PERFX@users.noreply.github.com>
2022-09-15 16:10:51 -0300

    :wrench: Adicionado o arquivo com as configurações

---
+++ js/config.js
@@ -0,0 +1,10 @@
+export let config = {
+  name: 'Rafael Fiuza',
+  nick: '@V1PERFX',
+  profession: 'Analista de Service Desk / Desenvolvedor Front-End',
+  imageURL: 'img/61255847.png',
+  links: [
+    { title: 'Hero Dash', url: 'https://github.com/V1PERFX/imersaoGameDev', icon: 'bi bi-controller' },
+    { title: 'UniCar', url: 'https://github.com/V1PERFX/unicar', icon: 'bi bi-window-fullscreen' },
+  ]
+}
