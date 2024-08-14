6c550c3803470eb01559639cdd7a6c01464737d3
Rafael Fiuza <61255847+V1PERFX@users.noreply.github.com>
2024-08-13 22:55:23 -0300

    :lipstick: Alterado ícone

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const config = {
   links: [
-    { title: 'Gerador de Senha', url: 'https://gh.rafaelviper.com.br/gerador-senha', icon: 'bi bi-star-fill' },
-    { title: 'CSV Viewer', url: 'https://gh.rafaelviper.com.br/csv', icon: 'bi bi-star' }
+    { title: 'Gerador de Senha', url: 'https://gh.rafaelviper.com.br/gerador-senha', icon: 'bi bi-github' },
+    { title: 'CSV Viewer', url: 'https://gh.rafaelviper.com.br/csv', icon: 'bi bi-github' }
   ]
 }
