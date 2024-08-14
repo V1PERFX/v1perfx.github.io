13b439c4e94248b7c33ca9b7fa3872a324547f0b
Rafael Fiuza <61255847+V1PERFX@users.noreply.github.com>
2024-08-13 23:02:47 -0300

    Adicionado mais links

--- js/config.js
+++ js/config.js
@@ -1,6 +1,11 @@
 export const config = {
   links: [
-    { title: 'Gerador de Senha', url: 'https://gh.rafaelviper.com.br/gerador-senha', icon: 'bi bi-github' },
-    { title: 'CSV Viewer', url: 'https://gh.rafaelviper.com.br/csv', icon: 'bi bi-github' }
+    { title: 'Gerador de Senha', url: 'https://gh.rafaelviper.com.br/gerador-senha/', icon: 'bi bi-github' },
+    { title: 'CSV Viewer', url: 'https://gh.rafaelviper.com.br/csv/', icon: 'bi bi-github' },
+    { title: '4Devs', url: 'https://www.4devs.com.br/', icon: 'bi bi-globe' },
+    { title: 'Gitmoji', url: 'https://gitmoji.dev/', icon: 'bi bi-globe' },
+    { title: 'IT Tools', url: 'https://it-tools.tech/', icon: 'bi bi-globe' },
+    { title: 'Temp Mail', url: 'https://temp-mail.org/', icon: 'bi bi-globe' },
+    { title: 'QuickRef.ME', url: 'https://quickref.me/', icon: 'bi bi-globe' }
   ]
 }
